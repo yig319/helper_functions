87c0b9ed41f65993273c8146c3f2ddf52f75ac94
Yichen Guo <yig319@nhi-gpu.cc.lehigh.edu>
2022-06-03 14:05:08 -0400

    add hist to show_images

--- visualization_functions.py
+++ visualization_functions.py
@@ -1,29 +1,31 @@
 import matplotlib.pyplot as plt
 
-def show_images(images, labels=None, img_per_row=8, img_height=1, show_axis=False, colorbar=False):
+def show_images(images, labels=None, img_per_row=8, img_height=1, show_hist=False, show_axis=False, colorbar=False):
     h = images[0].shape[1] // images[0].shape[0]*img_height + 1
     if not labels:
         labels = range(len(images))
-    fig, axes = plt.subplots(len(images)//img_per_row+1*int(len(images)%img_per_row>0), img_per_row, 
-                             figsize=(16, h*len(images)//img_per_row+1))
+        
+    n = 1
+    if show_hist: n +=1
+        
+    fig, axes = plt.subplots(n*len(images)//img_per_row+1*int(len(images)%img_per_row>0), img_per_row, 
+                             figsize=(16, n*h*len(images)//img_per_row+1))
     for i in range(len(images)):
-        if len(images) <= img_per_row:
-            axes[i%img_per_row].title.set_text(labels[i])
-            im = axes[i%img_per_row].imshow(images[i])
-            if colorbar:
-                fig.colorbar(im, ax=axes[i%img_per_row])
-            if not show_axis:
-                axes[i//img_per_row].axis('off')
-    
-            axes[i//img_per_row].axis('off')
-
+        
+        if len(images) <= img_per_row and not show_hist:
+            index = i%img_per_row
         else:
-            axes[i//img_per_row, i%img_per_row].title.set_text(labels[i])
-            im = axes[i//img_per_row, i%img_per_row].imshow(images[i])
-            if colorbar:
-                fig.colorbar(im, ax=axes[i//img_per_row, i%img_per_row])
-            if not show_axis:
-                axes[i//img_per_row, i%img_per_row].axis('off')
-            axes[i//img_per_row, i%img_per_row].axis('off')
-            
+            index = (i//img_per_row)*n, i%img_per_row
+
+        axes[index].title.set_text(labels[i])
+        im = axes[index].imshow(images[i])
+        if colorbar:
+            fig.colorbar(im, ax=axes[i%img_per_row])
+        if not show_axis:
+            axes[index].axis('off')
+
+        if show_hist:
+            index_hist = (i//img_per_row)*n+1, i%img_per_row
+            h = axes[index_hist].hist(images[i].flatten())
+
     plt.show()
