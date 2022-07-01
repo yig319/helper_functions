c77ce46a8a6f9d9de0d30b266a417bb84d981042
yig319 <yig319@lehigh.edu>
2022-07-01 12:09:34 -0700

    add show_plots function

--- visualization_functions.py
+++ visualization_functions.py
@@ -51,3 +51,31 @@ def show_images(images, labels=None, img_per_row=8, img_height=1, colorbar=False
             h = axes[index_hist].hist(img.flatten(), bins=hist_bins)
         
     plt.show()
+    
+
+def show_plots(ys, xs=None, labels=None, img_per_row=4, subplot_height=3):
+    
+#     assert type(ys) == np.ndarray, "use numpy array with shape=(n, y)"
+#     assert type(labels) == list or type(labels) == type(None), "use list for labels"
+    
+    if type(labels) == type(None): labels = range(len(ys))
+    
+    if type(xs) == type(None):
+        xs = []
+        for y in ys:
+            xs.append(np.linspace(0, len(y), len(y)+1))            
+#         xs, _ = np.meshgrid(np.linspace(0, 1, ys.shape[1]), np.ones((ys.shape[0])))
+        
+    fig, axes = plt.subplots(len(ys)//img_per_row+1*int(len(ys)%img_per_row>0), img_per_row, 
+                             figsize=(16, subplot_height*len(ys)//img_per_row+1))
+    for i, (x, y) in enumerate(zip(xs, ys)):
+        
+        if len(ys) <= img_per_row:
+            index = i%img_per_row
+        else:
+            index = (i//img_per_row), i%img_per_row
+
+        axes[index].title.set_text(labels[i])
+        im = axes[index].plot(x, y)
+    fig.tight_layout()
+    plt.show()
